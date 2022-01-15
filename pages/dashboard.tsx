588ef3142a9b9f3d2fd44aff5f1a73ee10162b2e
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2022-01-15 10:51:41 -0300

    Criando componente de permissão

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { destroyCookie } from "nookies"
 import { useContext } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
 import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
@@ -17,7 +18,9 @@ export default function Dashboard(){
     <>
       <h1>Dashboard: {user?.email}</h1>
 
-      { userCanSeeMetrics && <div>Métricas</div> }
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   )
 }
