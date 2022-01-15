4482a4c6aa5dd4eabd9616807e19d8b9a687dde5
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2022-01-15 11:44:49 -0300

    Broadcast de logout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,14 @@
 import { destroyCookie } from "nookies"
 import { useContext } from "react"
 import { Can } from "../components/Can"
-import { AuthContext } from "../contexts/AuthContext"
+import { AuthContext, signOut } from "../contexts/AuthContext"
 import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { AuthTokenError } from "../services/errors/AuthTokenError"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard(){
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   const userCanSeeMetrics = useCan({
     permissions: ['metrics.list']
@@ -17,6 +17,8 @@ export default function Dashboard(){
   return(
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
